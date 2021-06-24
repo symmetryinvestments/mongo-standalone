566274d7d1cc7e233cac0c973d7c969dd88c835d
Toby Hutton <toby@grusly.com>
2021-06-24 14:57:35 +1000

    Fix some potential issues flagged by review.
    - Use /++ .. +/ doc comments.
    - Fix potential off by one error for numChunks.
    - Split functions to improve modularity and consolidate a couple of
      Mongo queries.

--- source/kaleidic/mongo_gridfs.d
+++ source/kaleidic/mongo_gridfs.d
@@ -2,16 +2,15 @@ module kaleidic.mongo_gridfs;
 
 import kaleidic.mongo_standalone;
 
-// IN:     mongo            - active connection to the MongoDB instance.
-//         gridfsBucketName - full path to the GridFS parent of collections .files and .chunks.
-//         id               - the file object ID, i.e., it's "_id" value.
-// RETURN: The entire file contents in an array.
-// THROWS: File not found, or on corrupt properties.
+/++
+   IN:     mongo            - active connection to the MongoDB instance.
+           gridfsBucketName - full path to the GridFS parent of collections .files and .chunks.
+           id               - the file object ID, i.e., it's "_id" value.
+   RETURN: The entire file contents in an array.
+   THROWS: File not found, or on corrupt properties.
++/
 
 ubyte[] gridfsReadFile(MongoConnection mongo, const string gridfsBucketName, const ObjectId id) {
-    import std.outbuffer;
-
-    // Get the file size.
     OP_REPLY reply =
         mongo.query(gridfsBucketName ~ ".files", 0, 1, document([bson_value("_id", id)]));
     if (reply.errorCode != 0) {
@@ -20,17 +19,51 @@ ubyte[] gridfsReadFile(MongoConnection mongo, const string gridfsBucketName, con
     if (reply.documents.length != 1) {
         throw new Exception("Requested file not found.");
     }
+
     long fileSize = reply.documents[0]["length"].get!long;
     long chunkSize = reply.documents[0]["chunkSize"].get!long;
+
+    return gridfsReadFileById(mongo, gridfsBucketName, id, fileSize, chunkSize);
+}
+
+/++
+   IN:     mongo            - active connection to the MongoDB instance.
+           gridfsBucketName - full path to the GridFS parent of collections .files and .chunks.
+           name             - file name
+   RETURN: The entire file contents in an array.
+   THROWS: File not found, or if more than 1 file match name, or on corrupt properties.
++/
+
+ubyte[] gridfsReadFile(MongoConnection mongo, const string gridfsBucketName, const string name) {
+    OP_REPLY reply =
+        mongo.query(gridfsBucketName ~ ".files", 0, 2, document([bson_value("filename", name)]));
+    if (reply.errorCode != 0) {
+        throw new Exception(reply.errorMessage);
+    }
+    if (reply.documents.length != 1) {
+        throw new Exception("error: failed to match single file: '" ~ name ~ "'");
+    }
+
+    ObjectId id = reply.documents[0]["_id"].get!ObjectId;
+    long fileSize = reply.documents[0]["length"].get!long;
+    long chunkSize = reply.documents[0]["chunkSize"].get!long;
+
+    return gridfsReadFileById(mongo, gridfsBucketName, id, fileSize, chunkSize);
+}
+
+private ubyte[] gridfsReadFileById(MongoConnection mongo, const string gridfsBucketName,
+                                   const ObjectId id, long fileSize, long chunkSize) {
+    import std.outbuffer;
 
     OutBuffer contents = new OutBuffer();
     contents.reserve(fileSize);
 
-    // Loop for each chunk and append to our buffer.
-    int numChunks = cast(int)(fileSize / chunkSize + 1);
+    string collectionName = gridfsBucketName ~ ".chunks";
+
+    int numChunks = cast(int)((fileSize + chunkSize - 1) / chunkSize);
     for (int chunkIdx = 0; chunkIdx < numChunks; chunkIdx++) {
-        reply = mongo.query(gridfsBucketName ~ ".chunks", 0, 1,
-                            document([bson_value("files_id", id), bson_value("n", chunkIdx)]));
+        OP_REPLY reply = mongo.query(collectionName, 0, 1,
+                                     document([bson_value("files_id", id), bson_value("n", chunkIdx)]));
         if (reply.errorCode != 0) {
             throw new Exception(reply.errorMessage);
         }
@@ -43,20 +76,3 @@ ubyte[] gridfsReadFile(MongoConnection mongo, const string gridfsBucketName, con
     return contents.toBytes;
 }
 
-// IN:     mongo            - active connection to the MongoDB instance.
-//         gridfsBucketName - full path to the GridFS parent of collections .files and .chunks.
-//         name             - file name
-// RETURN: The entire file contents in an array.
-// THROWS: File not found, or if more than 1 file match name, or on corrupt properties.
-
-ubyte[] gridfsReadFile(MongoConnection mongo, const string gridfsBucketName, const string name) {
-    OP_REPLY reply =
-        mongo.query(gridfsBucketName ~ ".files", 0, 2, document([bson_value("filename", name)]));
-    if (reply.errorCode != 0) {
-        throw new Exception(reply.errorMessage);
-    }
-    if (reply.documents.length != 1) {
-        throw new Exception("error: failed to match single file: '" ~ name ~ "'");
-    }
-    return gridfsReadFile(mongo, gridfsBucketName, reply.documents[0]["_id"].get!ObjectId);
-}
