71a1d7b423a5468902c18c47175866a862bf5263
Denis Korzunov <denis@sil-dev.lxd>
2021-08-12 06:25:53 +0000

    Add batch retrieval of many files for GridFS

--- test/gridfs/test.sh
+++ test/gridfs/test.sh
@@ -7,19 +7,12 @@ if [[ ! -x "$(which rdmd)" && ! -x ./test_gridfs ]] ; then
   exit 1
 fi
 
+# test single retrieval for a large and a small file
 dd if=/dev/urandom of=small.bin bs=1K count=1
 dd if=/dev/urandom of=large.bin bs=1M count=100
 
-mongofiles -d test_files -r put small.bin
-mongofiles -d test_files -r put large.bin
-
-cleanup() {
-  mongofiles -d test_files delete small.bin
-  mongofiles -d test_files delete large.bin
-
-  rm -f small.bin large.bin
-}
-trap cleanup EXIT
+mongofiles --quiet -d test_files -r put small.bin
+mongofiles --quiet -d test_files -r put large.bin
 
 if [[ -x ./test_gridfs ]] ; then
   ./test_gridfs small.bin $(sha1sum small.bin | cut -d ' ' -f 1)
@@ -28,3 +21,24 @@ else
   rdmd -g -I../../source test_gridfs.d small.bin $(sha1sum small.bin | cut -d ' ' -f 1)
   rdmd -g -I../../source test_gridfs.d large.bin $(sha1sum large.bin | cut -d ' ' -f 1)
 fi
+
+# test retrieval of many small files
+dd if=/dev/urandom of=many.bin bs=1M count=100
+split -b 100000 --additional-suffix .bin many.bin many
+for file in `ls many*.bin`; do
+    mongofiles --quiet -d test_files -r put $file >/dev/null
+done
+
+if [[ -x ./test_gridfs_many ]] ; then
+    ls many*.bin | xargs ./test_gridfs_many
+else
+    ls many*.bin | xargs rdmd -g -I../../source test_gridfs_many.d
+fi
+
+cleanup() {
+    for file in `ls *.bin`; do
+	mongofiles --quiet -d test_files delete $file >/dev/null
+	rm -f $file
+    done
+}
+trap cleanup EXIT
